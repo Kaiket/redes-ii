75de7926fe0574b38cb503742d277f14409f8a3c
Kaiket <Enrique_cf@hotmail.com>
2014-03-05 23:17:31 +0100

    subiendo cambios a thread_handling

--- G-1301-03-P1/srclib/G-1301-03-P1-thread_handling.c
+++ G-1301-03-P1/srclib/G-1301-03-P1-thread_handling.c
@@ -2,7 +2,11 @@
  * Thread handling library for server connections
  */
 #include <stdio.h>
+#include <stdlib.h>
 #include <syslog.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <strings.h>
 #include "../includes/G-1301-03-P1-types.h"
 #include "../includes/G-1301-03-P1-thread_handling.h"
 
@@ -15,7 +19,7 @@ Thread_handler *thread_array = NULL;
 long unsigned int n_threads = 0;
 long unsigned int already_allocated = 0;
 long unsigned int array_first_free = 0;
-long threads_per_page = (sysconf(_SC_PAGE_SIZE) / sizeof (Thread_handler));
+long threads_per_page = 0;
 
 
 
@@ -23,21 +27,24 @@ long threads_per_page = (sysconf(_SC_PAGE_SIZE) / sizeof (Thread_handler));
  * Functions
  */
 
-
 int launch_thread(int client_sock, void* (*thread_routine) (void *arg)) {
     pthread_t thread;
 
     Thread_handler* t_aux = NULL;
     syslog(LOG_NOTICE, "Creating a thread to handle connection in socket %d", client_sock);
 
+    threads_per_page = (sysconf(_SC_PAGE_SIZE) / sizeof (Thread_handler));
+    
     if ((n_threads % threads_per_page == 0)) {
-        if (n_threads == already_reallocated) {
-            t_aux = (Thread_handler*) realloc(threads, (already_reallocated + threads_per_page) * sizeof (Thread_handler));
+        if (n_threads == already_allocated) {
+            t_aux = (Thread_handler*) realloc(thread_array, (already_allocated + threads_per_page) * sizeof (Thread_handler));
             if (!t_aux) {
                 syslog(LOG_ERR, "Could not allocate memory for the new thread (socket %d).", client_sock);
                 return ERROR;
             }
-            already_reallocated+=threads_per_page;
+            bzero(&t_aux[already_allocated], threads_per_page * sizeof (Thread_handler) ); /*initialize the newly reserved memory*/
+            already_allocated+=threads_per_page;
+            thread_array=t_aux;
         }
     }
     
@@ -58,6 +65,7 @@ int launch_thread(int client_sock, void* (*thread_routine) (void *arg)) {
     
     thread_array[array_first_free].thread_id=thread;
     array_first_free=NOT_REFRESHED;
+    n_threads++;
     
     return OK;
 }
@@ -66,23 +74,23 @@ int launch_thread(int client_sock, void* (*thread_routine) (void *arg)) {
  * Performs a non-blocking join for every active thread currently in the array.
  * Sets the value of array-first-free global variable to the first non-active thread after the join
  */
-void nbjoin_threads (void) {
+int nbjoin_threads (void) {
     int i;
     if (!thread_array) return;
     for (i=0; i<already_allocated; ++i) {
-        if (thread_array[i]->active) {
+        if (thread_array[i].active) {
             if (pthread_tryjoin_np(thread_array[i].thread_id, NULL) == 0) { /*thread terminated*/
-                syslog(LOG_NOTICE, "Thread %d in socket %d terminated\n", thread_array[i].thread_id, thread_array[i].socket);
+                syslog(LOG_NOTICE, "Thread %lu in socket %d terminated\n", thread_array[i].thread_id, thread_array[i].socket);
                 n_threads--;
-                thread_array[i]->active=0;
-                if (i<array_first_free) array_first_free==i;
+                thread_array[i].active=0;
+                if ((i<array_first_free) || (array_first_free < 0)) array_first_free=i;
             }
         }
         else {
-            if (i<array_first_free) array_first_free==i;
+            if ((i<array_first_free) || (array_first_free < 0)) array_first_free=i;
         }
     }
-    return;
+    return array_first_free;
 }
 
 /*
@@ -90,9 +98,9 @@ void nbjoin_threads (void) {
  * Use not recommended, it is better to perform a nbjoin_threads call and let it set the value.
  */
 int seek_array_first_free (void) {
-    int status;
+    int i;
     for (i=0; i<already_allocated; ++i) {
-        if (!thread_array[i]->active) {
+        if (!thread_array[i].active) {
             array_first_free=i;
             return array_first_free;
         }
